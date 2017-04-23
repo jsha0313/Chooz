495abec9b619d6ce595cd07f9f6fb45245d4f9cd
David Shin <davidshin@wustl.edu>
2017-04-22 23:20:56 -0500

    UI Change
    UI Change

--- www/js/index.js
+++ www/js/index.js
@@ -4,14 +4,14 @@
 
   @params object error
 */
-var displayError = function(error) {
+var displayError = function (error) {
   var message = "An error has occurred. Please try again.";
 
-    if ( error.hasOwnProperty('message') ) { 
-      message = error.message;
-    }
+  if (error.hasOwnProperty('message')) {
+    message = error.message;
+  }
 
-    return message;
+  return message;
 }
 
 var login = function () {
@@ -116,14 +116,14 @@ var tipNbudget = function () {
   console.log('tip n budget function');
   // Location();
 
-  var zipcode = document.getElementById('zipcode'); 
+  var zipcode = document.getElementById('zipcode');
   var locationOff = document.body.contains(zipcode);
   var salesTax = 0;
 
   if (!locationOff) {
     console.log('user enabled location services');
 
-    getZipcode( function (res) {
+    getZipcode(function (res) {
       /* 
         use a callback function so that
         this function does not go on until
@@ -151,7 +151,7 @@ var tipNbudget = function () {
 
   @params - String zipcode
 */
-var getSalesTax = function(zipcode) {
+var getSalesTax = function (zipcode) {
   console.log('get sales tax function');
   var xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
@@ -166,7 +166,7 @@ var getSalesTax = function(zipcode) {
   var url = "/tax?zipcode=" + zipcode;
 
   xhr.open("GET", url);
-  xhr.setRequestHeader("cache-control", "no-cache");  
+  xhr.setRequestHeader("cache-control", "no-cache");
   xhr.send();
 }
 
@@ -177,22 +177,22 @@ var getSalesTax = function(zipcode) {
 
   @params - function callback
 */
-var getZipcode = function(callback) {
+var getZipcode = function (callback) {
   var geocoder = new google.maps.Geocoder;
 
-  if ( navigator.geolocation ) {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       // console.log(position.address.postalCode);
       console.log(position);
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
-      };      
-      geocoder.geocode({'location': pos}, function(result, status) {
-        if ( status === 'OK') {
-          if ( result[0] ) {
+      };
+      geocoder.geocode({ 'location': pos }, function (result, status) {
+        if (status === 'OK') {
+          if (result[0]) {
             let postalCode = result[0].address_components.find(function (component) {
-                return component.types[0] == "postal_code";
+              return component.types[0] == "postal_code";
             });
             callback(postalCode);
           }
@@ -208,25 +208,29 @@ var getZipcode = function(callback) {
 // locate you.
 
 var map;
-var markers = [];
-var infoWindows = [];
+var venues = [];
+var venue_dict = {};
+var menu_dict = {};
+
 var initMap = function (bool = false) {
   if (!bool) return;
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 12
+    zoom: 15
   });
-  map.setOptions({styles: [
+  map.setOptions({
+    styles: [
       {
         featureType: 'poi.business',
-        stylers: [{visibility: 'off'}]
+        stylers: [{ visibility: 'off' }]
       },
       {
         featureType: 'transit',
         elementType: 'labels.icon',
-        stylers: [{visibility: 'off'}]
+        stylers: [{ visibility: 'off' }]
       }
-    ]});
+    ]
+  });
 
   var infoWindow = new google.maps.InfoWindow({ map: map });
 
@@ -251,7 +255,10 @@ var initMap = function (bool = false) {
   }
 
   // Update Foursquare locations every time where is a change in the map bounds
-  map.addListener('bounds_changed', function(e) {
+  map.addListener('zoom_changed', function (e) {
+    updateFQ();
+  });
+  map.addListener('drag_end', function (e) {
     updateFQ();
   });
 };
@@ -263,6 +270,111 @@ var handleLocationError = function (browserHasGeolocation, infoWindow, pos) {
     'Error: Your browser doesn\'t support geolocation.');
 };
 
+function menulist(title, id) {
+  if (venue_dict[id] === undefined) {
+    var menu_xhttp = getQueryXhttp(1, id);
+    menu_xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var menu_response = JSON.parse(menu_xhttp.responseText).response;
+        // console.log(menu_response);
+        if (menu_response.menu.menus.count != 0) {
+          var menu_entries_array = menu_response.menu.menus.items[0].entries.items;
+          var menu_entries_dict = {};
+          for (var i = 0; i < menu_entries_array.length; i++) {
+            var menu_entries = menu_entries_array[i];
+            menu_entries_dict[menu_entries.name] = menu_entries.entries.items;
+          }
+          venue_dict[id] = menu_entries_dict;
+
+          populateList(title, id);
+        }
+      }
+    }
+  } else {
+    populateList(title, id);
+  }
+};
+
+function populateList(title, id) {
+  // Populate the list with menu items
+  document.getElementById('list-title').innerHTML = title;
+  var menuList = document.getElementById('menu-list');
+
+  for (var menu_type in venue_dict[id]) {
+    var menu_header = document.createElement('ons-list-header');
+    menu_header.innerText = menu_type;
+
+    menuList.appendChild(menu_header);
+
+    // console.log(venue_dict[id][menu_type]);
+
+    for (var i=0; i< venue_dict[id][menu_type].length; i++) {
+      var menu = venue_dict[id][menu_type][i];
+      var menu_item = document.createElement('ons-list-item');
+      menu_item.innerText = menu.name;
+      // FIXME:
+      // menu price can be called with "menu.price"
+      // menu description can be called with "menu.description"
+      menuList.appendChild(menu_item);
+    }
+  }
+}
+
+function chooz() {    
+    var menu = [];
+    menu = document.getElementById("menulist");
+    console.log(document.getElementById("menulist")[0].checked+" "+document.forms[0]+" "+menu[0].checked+" "+menu.length);
+    var i;
+    for (i = 0; i < menu.length; i++) {
+        console.log("i: "+i);
+        if (menu[i].checked) {
+            console.log("inside checking");
+            console.log(menu[i].value);
+        }
+    }
+    
+}
+var showPopover = function (target) {
+    document
+        .getElementById('popover')
+        .show(target);
+};
+var hidePopover = function () {
+    document
+        .getElementById('popover')
+        .hide();
+};
+var showDialog = function (id) {
+    document
+        .getElementById(id)
+        .show();
+};
+var fromTemplate = function () {
+    var dialog = document.getElementById('dialog-3');
+
+    if (dialog) {
+        dialog.show();
+    }
+    else {
+        ons.createDialog('receipt.html')
+            .then(function (dialog) {
+                dialog.show();
+            });
+    }
+};
+var hideDialog = function (id) {
+    document
+        .getElementById(id)
+        .hide();
+};
+
+function showModal() {
+    var modal = document.querySelector('ons-modal');
+    modal.show();
+    setTimeout(function () {
+        modal.hide();
+    }, 2000);
+}
 ons.ready(function () {
   console.log('ons.ready firing');
   // Onsen UI is now initialized
@@ -295,7 +407,7 @@ ons.ready(function () {
     }
     if (page.id === 'tipNbudgetW/location' || page.id === 'tipNbudgetW/Olocation') {
       console.log('tip n budget');
-      page.querySelector('#confirm').onclick = function() {
+      page.querySelector('#confirm').onclick = function () {
         document.querySelector('#myNav')
           .pushPage('search.html', { data: { title: 'search' } })
           .then(function () {
@@ -304,45 +416,35 @@ ons.ready(function () {
           });
       }
     };
+    if (page.id === 'search') {
+            page.querySelector('#searchButton').onclick = function () { //FIX ME: temporary trigger button as Setting. modify this to trigger when marker is clicked
+                document.querySelector('#myNav').pushPage('menulist.html', { data: { title: 'Menulist' } })
+                    .then(function () {
+                        menulist();
+                    });
+            };
+            page.querySelector('#settingButton').onclick = function () { //FIX ME: temporary trigger button as Setting. modify this to trigger when marker is clicked
+                document.querySelector('#myNav').pushPage('setting.html', { data: { title: 'Setting' } })
+                    .then(function () {
+                        setting();
+                    });
+            };
+        }
   });
 });
 
-var updateFQ = function() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-         // Action to be performed when the document is read;
-          var response = JSON.parse(xhttp.responseText).response;
-          var objects = response.groups[0].items;
-          console.log(objects);
-          for (var i=0; i<markers.length; i++) {
-            markers[i].setMap(null);
-          }
-          markers = [];
-          for (var i=0; i<objects.length; i++) {
-
-            var marker = new google.maps.Marker({
-              position: {lat: objects[i].venue.location.lat, lng: objects[i].venue.location.lng},
-              map: map,
-              title: objects[i].venue.name,
-            });
-
-            markers.push(marker);
-          }
-
-      }
-  };
+function getQueryXhttp(type, venue_id) {
   var date = new Date();
   var dd = date.getDate();
-  var mm = date.getMonth()+1; //January is 0
+  var mm = date.getMonth() + 1; //January is 0
   var yyyy = date.getFullYear();
-  if(dd<10){
-      dd='0'+dd;
-  } 
-  if(mm<10){
-      mm='0'+mm;
-  } 
-  var today = yyyy+mm+dd;
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  var today = yyyy + mm + dd;
   var client_id = "VZP5RZFVTJRAPIASNSAEHBOBU12DK3WC4SQQNHZCAG3FAXT2";
   var client_secret = "XDVSW4E3LHM3NM31BYPOGNSW21MFDXF3BDPJ0YBSPGCGCVQX";
   var center = map.getCenter().toUrlValue(14);
@@ -352,35 +454,119 @@ var updateFQ = function() {
     sw_lon = map.getBounds().getSouthWest().lng();
     ne_lat = map.getBounds().getNorthEast().lat();
     ne_lon = map.getBounds().getNorthEast().lng();
-  } 
+  }
   var radius = getRadius(sw_lat, sw_lon, ne_lat, ne_lon);
 
-  // var http_link = "https://api.foursquare.com/v2/venues/search?client_id="+client_id+
-  //                 "&client_secret="+client_secret+"&v="+today+"&intent=checkin"+
-  //                 "&ll="+center+"&sw="+sw+"&ne="+ne+
-  //                 "&categoryId=4d4b7105d754a06374d81259"+
-  //                 "&limit=10";
-  var http_link = "https://api.foursquare.com/v2/venues/explore?client_id="+client_id+"&client_secret="+client_secret+
-                  "&ll="+center+
-                  "&v="+today+
-                  "&radius="+radius+
-                  "&section=food"+
-                  "&limit=10";
-  xhttp.open("GET", http_link, true);
+  xhttp = new XMLHttpRequest();
+
+  var query_link;
+  if (type == 0) {
+    query_link = "https://api.foursquare.com/v2/venues/explore" +
+      "?client_id=" + client_id + "&client_secret=" + client_secret +
+      "&ll=" + center +
+      "&v=" + today +
+      "&radius=" + radius +
+      "&section=food" +
+      "&limit=10";
+  } else if (type == 1) {
+    query_link = "https://api.foursquare.com/v2/venues/" + venue_id + "/menu" +
+      "?client_id=" + client_id + "&client_secret=" + client_secret +
+      "&v=" + today;
+  }
+  xhttp.open("GET", query_link, true);
   xhttp.send();
 
+  return xhttp;
+}
+
+var updateFQ = function () {
+  var explore_xhttp = getQueryXhttp(0);
+  explore_xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var explore_response = JSON.parse(explore_xhttp.responseText).response;
+
+      var items = explore_response.groups[0].items;
+
+      for (var i = 0; i < venues.length; i++) {
+        venues[i].marker.setMap(null);
+      }
+
+      venues = [];
+      for (var i = 0; i < items.length; i++) {
+        var venue =
+          {
+            marker: new google.maps.Marker({
+              position: { lat: items[i].venue.location.lat, lng: items[i].venue.location.lng },
+              // position: items[i].geometry.location,
+              map: map,
+              title: items[i].venue.name,
+              icon: {
+                url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+                anchor: new google.maps.Point(10, 10),
+                scaledSize: new google.maps.Size(10, 17)
+              },
+              store_id: items[i].venue.id
+            }),
+            id: items[i].venue.id,
+          }
+        console.log(venue.marker.title+": "+venue.id);
+        venues.push(venue);
+
+        infoWindow = new google.maps.InfoWindow({ map: map });
+        venue.marker.addListener('click', function () {
+
+          var div = document.createElement("DIV");
+          var s = document.createElement("STRONG");
+          s.innerHTML = this.title;
+          var br = document.createElement("BR");
+          var id = document.createElement("P");
+          id.id = "venueId";
+          id.innerHTML = this.store_id;
+          var button = document.createElement("BUTTON");
+          button.type = "button";
+          var button_text = document.createTextNode("Click me");
+
+          div.appendChild(s);
+          div.appendChild(br);
+          button.appendChild(button_text);
+          div.appendChild(button);
+          div.appendChild(id);
+
+          div.addEventListener('click', function(div) {
+            return function() {
+              console.log(this);
+              openMenuList(div.childNodes[0].innerHTML, div.childNodes[div.childNodes.length - 1].innerHTML);
+
+            }
+            }(div));
+
+          infoWindow.setContent(div);
+          infoWindow.open(map, this);
+        });
+
+        venues.push(venue);
+      }
+    }
+  }
+};
+
+function openMenuList(title, id) {
+  document.querySelector('#myNav').pushPage('menulist.html', { data: { title: 'Menulist' } })
+      .then(function () {
+          menulist(title, id);
+      });
 }
 
 function getRadius(lat1, lon1, lat2, lon2) {
-    var R = 6378.137; // Radius of earth in KM
-    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
-    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+  var R = 6378.137; // Radius of earth in KM
+  var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+  var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-    Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-    return Math.round(d * 1000); // meters
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return Math.round(d * 1000); // meters
 }
 // (function(){   
 
@@ -403,3 +589,4 @@ function getRadius(lat1, lon1, lat2, lon2) {
 //     }
 //   });
 // });
+
